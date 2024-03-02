71a48f8118ec5e0e5a067bf9a0778fa540916e13
Imam Usmani <imam@e.email>
2024-03-02 22:26:07 +0100

    fix

--- src/calibrolino.py
+++ src/calibrolino.py
@@ -105,13 +105,16 @@ class CalibreDBReader(object):
         # authors_names = {author['id']: author['name'] for author in self._tables['authors']}
 
         data_names = dict()
-        datas = 'series', 'tags', 'authots'
+        datas = 'series', 'tags', 'authors'
         if self._status_is_defined:
             datas.append(self._status_table_name)
         for data in datas:
             data_names[data] = dict()
             for row in self._tables[data]:
-                data_names[data][row['id']] = row.get('name', row.get('value'))
+                if 'name' in row.keys():
+                    data_names[data][row['id']] = row['name']
+                else:
+                    data_names[data][row['id']] = row['value']
         print(data_names)
 
         # series = {serie_link['book']: series_name[serie_link['series']] for serie_link in self._tables['books_series_link']}
