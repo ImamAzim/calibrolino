4b6f0c48a0775bc3984ab0a7b53a21ddac4edcaa
ImamAzim <imam@e.email>
2024-08-02 11:01:28 +0200

    parse arg for verbose

--- src/calibrolino/apps.py
+++ src/calibrolino/apps.py
@@ -1,11 +1,20 @@
 from calibrolino.views import CalibrolinoShellView
 import logging
+import argparse
 
 
 def start_calibrolino_shell():
+    parser = argparse.ArgumentParser(
+            prog='calibrolino',
+            description='sync calibre library to mytolino',
+            )
+    parser.add_argument(
+            '-v',
+            '--verbose',
+            action='store_true',
+            )
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
     view = CalibrolinoShellView()
     view.start()
-
-def start_debug_mode():
-    logging.basicConfig(level=logging.INFO)
-    start_calibrolino_shell()
