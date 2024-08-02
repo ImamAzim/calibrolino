31c76629af203b0a775e9bf8c59bfdcbd74d4f29
ImamAzim <imam@e.email>
2024-08-02 10:55:43 +0200

    add app debug

--- src/calibrolino/apps.py
+++ src/calibrolino/apps.py
@@ -1,6 +1,11 @@
 from calibrolino.views import CalibrolinoShellView
+import logging
 
 
 def start_calibrolino_shell():
     view = CalibrolinoShellView()
     view.start()
+
+def start_debug_mode():
+    logging.basicConfig(level=logging.INFO)
+    start_calibrolino_shell()
