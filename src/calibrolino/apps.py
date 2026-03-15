83cab933af5c9b732f7823f73aa79dd47bf50a16
Imam Usmani <imam@e.email>
2026-03-15 21:12:21 +0100

    init controler

--- src/calibrolino/apps.py
+++ src/calibrolino/apps.py
@@ -1,6 +1,29 @@
-from calibrolino.views import CalibrolinoShellView
 import logging
 import argparse
+
+
+from calibrolino.views import CalibrolinoShellView
+from calibrolino.controllers import CalibrolinoController
+from calibrolino.interfaces import View, Controller
+
+
+class CalibrolinoShellApp(object):
+
+    """calibrolino app in a shell"""
+
+    def __init__(self):
+        """init mvc model """
+        self._view = CalibrolinoShellView()
+        controller = CalibrolinoController()
+        table: BaseTable = Table(self._gui)
+        self._gui.table = table
+        self._gui.place_card_on_table
+
+    def start(self):
+        """start the pycard app
+
+        """
+        self._gui.run()
 
 
 def start_calibrolino_shell():
--- src/calibrolino/controllers.py
+++ src/calibrolino/controllers.py
@@ -1,4 +1,10 @@
-class CalibrolinoController():
+from calibrolino.interfaces import Controller, View
+
+
+class CalibrolinoController(Controller):
 
     """controller of calibrolino in mvc arch"""
-    pass
+
+    def __init__(self, view: View):
+        Controller.__init__(self)
+        self._view = view
