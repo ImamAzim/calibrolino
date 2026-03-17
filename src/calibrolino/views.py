b9ef871ddda776c54a44817abd22dec0d30c2073
Imam Usmani <imam@e.email>
2026-03-17 23:21:21 +0100

    fix syntax

--- src/calibrolino/views.py
+++ src/calibrolino/views.py
@@ -85,7 +85,7 @@ class CalibrolinoShellView(View):
         save them for next time
 
         """
-        partner_list = self.controller.partners
+        partners_list = self.controller.partners
         for i, partner in enumerate(partners_list):
             print(f'{i}: {partner}')
         id_partner = input('please enter the number of the tolino partner you want:')
