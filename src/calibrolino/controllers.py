e4ef24fc66816e7f8a743fff32ef6402800cf99a
Imam Usmani <imam@e.email>
2026-04-04 23:41:32 +0200

    fix local id var

--- src/calibrolino/controllers.py
+++ src/calibrolino/controllers.py
@@ -270,7 +270,7 @@ class CalibrolinoController(Controller):
                 else:
                     local_id = self._calibre_db.online_books.get(online_id)
                     if local_id:
-                        self._calibre_db.rm_online_id(book_id)
+                        self._calibre_db.rm_online_id(local_id)
                         self._calibre_db.commit()
                     msg = f'{book_title} has been deleted'
                     self._view.showinfo(msg)
