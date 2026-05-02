f14d6fb7074837eb1972846d901927a96bdae710
Imam Usmani <imam@e.email>
2026-05-02 21:39:08 +0200

    use get tags in push

--- src/calibrolino/controllers.py
+++ src/calibrolino/controllers.py
@@ -147,7 +147,9 @@ class CalibrolinoController(Controller):
         # TODO: push data
         local_revision = self._varbox.revision
         local_patches = self._varbox.patches
-        tags_to_add, tags_to_delete = self._calibre_db.ge
+        tags_to_add, tags_to_delete = self._calibre_db.get_tags_to_sync(
+            local_patches, books_to_push
+        )
         pass
 
     def _pull(self, force=False):
