8208e0158e494b5e2f8eaa95356bfed082c66a8d
Imam Usmani <imam@e.email>
2026-05-06 21:01:48 +0200

    format

--- src/calibrolino/__init__.py
+++ src/calibrolino/__init__.py
@@ -1,10 +1,11 @@
 from varboxes import VarBox
 
+
 default_values = dict(
-        last_push_date=0,
-        patches={},
-        revision='',
-        )
+    last_push_date=0,
+    patches={},
+    revision='',
+)
 varbox = VarBox('calibrolino')
 for key, value in default_values.items():
     if not hasattr(varbox, key):
